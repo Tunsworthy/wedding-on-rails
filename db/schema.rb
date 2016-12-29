6790b9c577cf09ea62970c39886fa6b0b7c98b18
Tom Unsworth <mail@tomunsworth.net>
2016-12-29 22:02:36 +0000

    Updated Event form to allow for editing change DB to PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150102105835) do
+enable_extension "plpgsql"
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
