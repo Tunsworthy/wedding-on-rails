eb8fd03d68a7f9034de132ceb9eace980fa49128
Tom Unsworth <mail@tomunsworth.net>
2017-01-03 22:49:52 +0000

    removed some files that got places wrongly..
    made some carrierwave setting changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,5 +5,4 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     :region                 => ENV['AWS_REGION']
   }
-  config.fog_directory = ENV['AWS_S3_BUCKET']
 end
